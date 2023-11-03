5cac2b7f4bde21714ef9bc31b38699e85023d56c
Richard Green <richard.green@unsw.edu.au>
2023-11-04 08:19:14 +1100

    ssh via proxy-jump to workload app instance script

---
+++ scripts/040-ssh-to-workload-app-instance-1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ssh agent passes our ssh private key to other hosts when we ssh to them
+# using the -A flag. We need to ensure only one ssh agent is running.
+
+unset SSH_AGENT_PID
+unset SSH_AUTH_SOCK
+
+killall ssh-agent 2>/dev/null || true
+eval "$(ssh-agent -s)"
+ssh-add -D
+ssh-add ssh-private_key.pem
+
+# make sure you run scripts/020-start-ssl-agent-in-cloudshell.sh first to start the ssh agent
+
+
+WORKLOAD_APP_INSTANCE_1_PRIVATE_IP=$(aws cloudformation describe-stacks \
+  --stack-name inf3701-extra-points \
+  --query 'Stacks[0].Outputs[?OutputKey==`WorkloadAppInstance1PrivateIp`].OutputValue' \
+  --output text)
+
+BASTION_HOST_PUBLIC_IP=$(aws cloudformation describe-stacks \
+  --stack-name inf3701-extra-points \
+  --query 'Stacks[0].Outputs[?OutputKey==`BastionHostPublicIp`].OutputValue' \
+  --output text)
+
+ssh -A -i ssh-private_key.pem -J ec2-user@$BASTION_HOST_PUBLIC_IP ec2-user@$WORKLOAD_APP_INSTANCE_1_PRIVATE_IP
+
