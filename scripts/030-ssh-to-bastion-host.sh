7781ff6a4e82572bc20a2b5663a2bb4fc7810e12
Richard Green <richard.green@unsw.edu.au>
2023-11-04 08:07:09 +1100

    outputs for private ip addressees of application ec2s

--- scripts/030-ssh-to-bastion-host.sh
+++ scripts/030-ssh-to-bastion-host.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# ssh agent passes our ssh private key to other hosts when we ssh to them
+# using the -A flag. We need to ensure only one ssh agent is running.
+
+unset SSH_AGENT_PID
+unset SSH_AUTH_SOCK
+
+killall ssh-agent 2>/dev/null || true
+eval "$(ssh-agent -s)"
+ssh-add -D
+ssh-add ssh-private_key.pem
+
 # make sure you run scripts/020-start-ssl-agent-in-cloudshell.sh first to start the ssh agent
 
 BASTION_HOST_PUBLIC_IP=$(aws cloudformation describe-stacks \
