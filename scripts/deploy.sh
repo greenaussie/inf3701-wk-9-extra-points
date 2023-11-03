7982ceaaf14f3320a91042e5b0b2c641c762cd36
Richard Green <richard.green@unsw.edu.au>
2023-11-03 19:38:28 +1100

    update security group

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,3 +9,12 @@ aws cloudformation deploy \
   --template-file cloudformation-templates/infrastructure.yml \
   --stack-name inf3701-extra-points \
   --capabilities CAPABILITY_IAM
+
+# Save ssh key to file
+aws cloudformation describe-stacks \
+  --stack-name inf3701-extra-points \
+  --query 'Stacks[0].Outputs[?OutputKey==`SSHKey`].OutputValue' \
+  --output text > ssh-key.pem
+
+
+ aws ssm get-parameter --name /ec2/keypair/key-0cdb6bd6ad233274e --with-decryption --query Parameter.Value --output text
